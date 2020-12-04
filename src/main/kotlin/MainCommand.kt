eb9165ab5554d1d9c8ec1e0838ebc63f78ba3f27
KiRist <37296174+KiRist-code@users.noreply.github.com>
2020-12-04 10:43:35 +0900

    Add files via upload

---
+++ src/main/kotlin/MainCommand.kt
@@ -0,0 +1,32 @@
+package kr.heartpattern.kotlinpluginsample
+
+import com.sun.tools.jdi.Packet
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.command.PluginCommand
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.plugin.java.JavaPlugin
+
+class MainCommand:Main(), Listener, CommandExecutor{
+    @EventHandler
+    public fun joinEvent(event: PlayerJoinEvent){
+        event.player.sendMessage("Hello user! Welcome to my Server")
+    }
+
+    override fun onCommand(sender: CommandSender?, command: Command?, label: String?, args: Array<out String>?): Boolean {
+        if (sender is Player) {
+            if (command != null) {
+                when (command.name) {
+                    "hello" -> {
+                        sender.sendMessage("Hello " + sender.name + "!")
+                    }
+                }
+            }
+        }
+        return true
+    }
+}
