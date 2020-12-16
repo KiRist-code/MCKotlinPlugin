03c248ecf974c8caa2838cb36b5a58b90dda49fe
KiRist <37296174+KiRist-code@users.noreply.github.com>
2020-12-16 14:31:40 +0900

    Update MainCommand.kt

--- src/main/kotlin/MainCommand.kt
+++ src/main/kotlin/MainCommand.kt
@@ -1,14 +1,25 @@
 package kr.heartpattern.kotlinpluginsample
 
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.World
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
-import org.bukkit.command.PluginCommand
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerMoveEvent
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.util.noise.SimplexOctaveGenerator
+import java.sql.Time
+import java.time.Instant
+import kotlin.random.Random
+import org.bukkit.inventory.ShapedRecipe
 
 class MainCommand:Main(), Listener, CommandExecutor{
     @EventHandler
@@ -25,6 +36,16 @@ class MainCommand:Main(), Listener, CommandExecutor{
                     }
                     "help" -> {
                         sender.sendMessage("sry, there is no help... so can u wait?")
+                    }
+                    "day" -> {
+                        if(!setDay) {
+                            setDay = true
+                            sender.sendMessage("Days activated")
+                        }
+                        else {
+                            setDay = false
+                            sender.sendMessage("Days off")
+                        }
                     }
                 }
             }
